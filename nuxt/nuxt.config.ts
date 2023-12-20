cc25d4b7ccabe97db592f4973f6cb533eed62656
Andrii Lytvyn <lytvyn349@gmail.com>
2023-12-20 04:08:51 +0200

    build: add nuxt modules: tailwind, image, icons, sanity

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    'nuxt-icons',
+    '@nuxtjs/sanity'
+  ],
+  sanity: {
+    // TODO: boostrap sanity
+  }
 })
