298a5f1bd4c84926518083cb84a11a2f93be6971
Andrii Lytvyn <lytvyn349@gmail.com>
2023-12-27 00:52:32 +0200

    refactor: remove time info

--- nuxt/components/Player.vue
+++ nuxt/components/Player.vue
@@ -8,42 +8,10 @@
       class="w-4 h-4"
       @click="isPlaying ? pause() : play()"
     />
-    <span>
-      <span
-        >{{
-          `${new Date(playbackPosition).getMinutes()}:${new Date(
-            playbackPosition
-          ).getSeconds()}`
-        }}
-      </span>
-      <span>/</span>
-      <span>
-        {{
-          `${new Date(duration!).getMinutes()}:${new Date(
-            duration!
-          ).getSeconds()}`
-        }}
-      </span>
-    </span>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useSound } from '@vueuse/sound'
 const { play, pause, isPlaying, duration } = useSound('/track.mp3')
-const playbackPosition = ref<number>(0)
-
-setInterval(() => {
-  if (isPlaying.value) {
-    playbackPosition.value += 1000
-  }
-
-  if (
-    playbackPosition.value - 1000 ===
-    Math.floor(duration.value! / 1000) * 1000
-  )
-    playbackPosition.value = 1000
-
-  console.log(playbackPosition.value)
-}, 1000)
 </script>
