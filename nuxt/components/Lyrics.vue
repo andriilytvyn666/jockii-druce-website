e3e8b810b16c1164d1abce5f09c9586e170c9256
Andrii Lytvyn <lytvyn349@gmail.com>
2023-12-31 01:52:45 +0200

    fix: update lyrics & twitter paddings

--- nuxt/components/Lyrics.vue
+++ nuxt/components/Lyrics.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex overflow-visible sm:overflow-y-scroll sm:max-h-screen sm:py-6 pb-24"
+    class="flex overflow-visible sm:overflow-y-scroll sm:max-h-screen sm:py-6 pb-8"
   >
     <span>
       <PortableText :key="currentTrack.name" :value="currentTrack.lyrics" />
