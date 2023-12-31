d25aeab78bde047bbc5db442b913a857afc6f798
Andrii Lytvyn <lytvyn349@gmail.com>
2023-12-31 16:08:00 +0200

    feat: add metadata

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -8,3 +8,40 @@
     </DevOnly>
   </div>
 </template>
+
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'jockii druce',
+  description: 'шо ви браття?',
+})
+
+useHeadSafe({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#282828',
+    },
+  ],
+})
+</script>
